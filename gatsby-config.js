da298405070b5a7fcb60d0fc78894afd55fcd561
William <wtpwinberg@gmail.com>
2018-08-30 10:29:26 -0700

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Advanced Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Free Code Camp!</h1>
+    <p>Welcome to my "Hello World" site.</p>
+    <p>I'm William, a developer from Los Angles</p>
+    <p>Now to hackathon something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
